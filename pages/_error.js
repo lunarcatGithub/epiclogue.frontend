8be9a01f05cd38c81b4ddd4f9ab1ac8f66df1d7f
taypark <interrupt@kakao.com>
2021-03-12 14:51:13 +0900

    fix typo (getInitialProps)

--- pages/_error.js
+++ pages/_error.js
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 export default function Error({ statusCode }) {
   return <div>{statusCode ? 'server error' : 'client error'}</div>;
 }
-Error.getIntialProps = ({ res, err }) => {
+Error.getInitialProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
   return { statusCode };
 };
