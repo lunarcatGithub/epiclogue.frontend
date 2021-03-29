956378e709cdf30879595e15cb6c23207d6fe217
taypark <interrupt@kakao.com>
2021-03-29 16:44:41 +0900

    reinstall nodemodules when after install

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 cd /home/ubuntu/epiclogue.frontend
+# node_modules 재설치
+rm -rf node_modules
+yarn install
 # 도커 이미지 클린 셋업
 docker-compose stop
 docker container rm -f $(docker ps -aq)
