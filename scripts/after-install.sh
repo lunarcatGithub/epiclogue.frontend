08f8528fd576c67e0b9fce5580509354bb0e922d
taypark <interrupt@kakao.com>
2021-04-10 18:21:04 +0900

    fix after-install script

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 cd /home/ubuntu/epiclogue.frontend
-rm -rf node_modules
-yarn install
 # 도커 이미지 클린 셋업
-docker-compose stop
-docker container rm -f $(docker ps -aq)
+docker-compose down
 docker-compose up --build -d
-  
