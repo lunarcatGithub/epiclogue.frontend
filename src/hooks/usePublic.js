cf9c2ff1df438386677e2cf1c1864e1a2626a5a0
TaeHee <klimes888@gmail.com>
2021-04-11 21:13:16 +0900

    clean console

--- src/hooks/usePublic.js
+++ src/hooks/usePublic.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 
 export default function usePublic({ pub, _writer, sessionId }) {
   const [_public, setPublic] = useState();
+  console.log(pub)
   // 공개 비공개 함수
   const contentPubHandler = () => {
     if (_writer !== sessionId && pub === 0) {
