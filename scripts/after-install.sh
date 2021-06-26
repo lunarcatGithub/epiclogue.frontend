1e5ecb78421063f45108bca2b9780dcc0821156a
taypark <interrupt@kakao.com>
2021-06-26 17:07:08 +0900

    Fix pipeline scripts and spec file

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 cd /home/ubuntu/epiclogue.frontend
-rm -rf node_modules
-npm install
 # 도커 이미지 클린 셋업
 docker-compose down
 docker-compose up --build -d
--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ -d /home/ubuntu/epiclogue.frontend ]
-then
-    sudo rm -rf /home/ubuntu/epiclogue.frontend
-fi
-mkdir -p /home/ubuntu/epiclogue.frontend
-cp -r /home/ubuntu/envs/epiclogue.ssl /home/ubuntu/epiclogue.frontend
+# SSL 인증서 및 .env 파일 복사
+cp -r /home/ubuntu/epiclogue.ssl /home/ubuntu/epiclogue.frontend
 cp /home/ubuntu/envs/.env* /home/ubuntu/epiclogue.frontend
